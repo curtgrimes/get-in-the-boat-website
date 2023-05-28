ecfbbfbdba06ff1100c9e0a0a0e22db2efd9a8d2
Curt Grimes <curt@curtgrimes.com>
2023-05-28 13:33:15 -0500

    test

--- app.vue
+++ app.vue
@@ -3,6 +3,6 @@
     class="w-screen h-screen flex items-center justify-center text-center gap-y-4 flex-col text-2xl pb-10"
   >
     <strong>Get in the Boat</strong>
-    Coming Soon
+    Coming Soon Test
   </div>
 </template>
