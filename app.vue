27c5a92c984989d180400f7de7bbfcd919707498
Curt Grimes <curt@curtgrimes.com>
2023-05-28 13:26:47 -0500

    Add Tailwind. Add minimal 'coming soon' page.

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div
+    class="w-screen h-screen flex items-center justify-center text-center gap-y-4 flex-col text-2xl pb-10"
+  >
+    <strong>Get in the Boat</strong>
+    Coming Soon
   </div>
 </template>
