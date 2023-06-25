16b549969245ac09492ef180d6574cfa6fcd9de7
Curt Grimes <curt@curtgrimes.com>
2023-06-24 22:32:23 -0500

    Add start of formats section

--- components/section/Wrapper.vue
+++ components/section/Wrapper.vue
@@ -1,13 +1,29 @@
-<script setup>
+<script setup lang="ts">
 import { twMerge } from "tailwind-merge";
 /**
  * A SectionWrapper that sections can use to have a consistent appearance
  */
+
+withDefaults(
+  defineProps<{
+    withTextGutter?: Boolean;
+  }>(),
+  {
+    withTextGutter: () => true,
+  }
+);
 </script>
 
 <template>
-  <section :class="twMerge('py-120', $attrs.class)">
-    <div class="mx-auto max-w-screen-xl px-text-gutter">
+  <section :class="twMerge('py-120', ($attrs.class || '') as string)">
+    <div
+      :class="
+        twMerge(
+          'mx-auto max-w-screen-xl',
+          $props.withTextGutter && 'px-text-gutter'
+        )
+      "
+    >
       <slot />
     </div>
   </section>
