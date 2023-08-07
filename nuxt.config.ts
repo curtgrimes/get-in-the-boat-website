3a1a04a4ed042553873cb581d6e80d43df6d433f
Curt Grimes <curt@curtgrimes.com>
2023-08-06 22:33:45 -0500

    Fix Netlify build OOM issue
    From advice at https://answers.netlify.com/search?q=%22non-zero%20exit%20code:%20134%22

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,4 +17,17 @@ export default defineNuxtConfig({
       },
     ],
   ],
+  // Workaround for Netlify OOM deploy issue
+  // See https://answers.netlify.com/t/javascript-heap-out-of-memory-when-trying-to-build-a-nuxt-app/93138/14
+  postcss: {
+    plugins: {
+      'tailwindcss/nesting': {},
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano:
+        process.env.NODE_ENV === 'production'
+          ? { preset: ['default', { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
+     },
+  }
 });
