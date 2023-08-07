9fc90f9978717c97c728d287169e5d55b03bc189
Curt Grimes <curt@curtgrimes.com>
2023-08-06 23:21:25 -0500

    Add favicon

--- app.vue
+++ app.vue
@@ -9,6 +9,12 @@ useHead({
       "Get in the Boat - A Memoir of Love, Loss, and the Dance with Dementia";
     return subpageName ? `${subpageName} - ${mainTitle}` : mainTitle;
   },
+  link: [
+    {
+      rel: "icon",
+      href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚣</text></svg>",
+    },
+  ],
 });
 </script>
 
