0024ec6720b820a7c2c3db0b28e7ff524c348fa3
Curt Grimes <curt@curtgrimes.com>
2023-08-08 09:14:07 -0500

    Improve boat background positioning

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@ const navbarAnchors = useNavbarAnchors();
   <div>
     <div class="relative z-0 bg-primary text-neutral-light">
       <img
-        class="absolute -z-10 h-full w-full object-cover opacity-25 mix-blend-luminosity"
+        class="absolute -z-10 h-full w-full object-cover opacity-25 mix-blend-luminosity object-top"
         src="~/assets/img/hero-bg.jpg"
       />
       <Navbar />
